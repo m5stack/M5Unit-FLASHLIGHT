5dd85f098497b4d415e807a10a238b53cd978349
GOB <goblin52@gmail.com>
2026-05-08 12:56:28 +0900

    Add FlashDemo example for UnitFlashLight

---
+++ examples/UnitUnified/UnitFlashLight/FlashDemo/FlashDemo.ino
@@ -0,0 +1,14 @@
+/*
+ * SPDX-FileCopyrightText: 2026 M5Stack Technology CO LTD
+ *
+ * SPDX-License-Identifier: MIT
+ */
+/*
+  Example using M5UnitUnified for UnitFlashLight (AW3641E, SKU:U152)
+
+  WARNING: Set the S1 switch on UnitFlashLight to the Flash side
+           for brightness/timeout selection to take effect.
+           When S1 is on the Torch side, the LED is lit at a fixed
+           current (~214 mA) regardless of pulse commands.
+*/
+#include "main/FlashDemo.cpp"
